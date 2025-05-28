6d653a16e8f81169a7e42782b2c4287b1cb7ce80
0xjericlloyd <0xjericlloyd@gmail.com>
2025-05-28 08:30:41 +0800

    Deleted zone-book.ts

--- src/lib/types/zone-book.ts
+++
@@ -1,9 +0,0 @@
-import { Area } from "./area";
-import { Consumer } from "./consumer";
-
-export type ZoneBook = {
-  id: string;
-  zone: Area["zone"];
-  book: Area["book"];
-  consumers: Array<Consumer>;
-};
